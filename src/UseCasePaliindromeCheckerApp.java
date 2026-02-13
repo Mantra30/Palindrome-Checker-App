3880070fb16bf610aa64673fca99df9adbdcd41f
Mantra30 <mantrahirani30@gmail.com>
2026-02-13 11:27:18 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/UseCasePaliindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class UseCasePaliindromeCheckerApp {
+    public static void main (String[] args){
+        System.out.println("Paliindrome Checker App");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
